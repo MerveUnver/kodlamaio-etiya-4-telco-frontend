d8bffef2a40912be4404a5d1a1c9d599d928bb38
Merve Ünver <merveunvr123@gmail.com>
2022-09-14 10:38:59 +0300

    last commit

---
+++ src/app/core/auth/guards/login.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.authService.isAuthenticated) {
+      this.router.navigateByUrl('login');
+      //this.toastrService.warning('Please sign in to continue!')
+      return false;
+    }
+
+    //this.toastrService.success('Please sign in to continue!')
+    return true;
+  }
+}
